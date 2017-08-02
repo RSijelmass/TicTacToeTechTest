6acdb09a7942a9223f71e636c3770470226678dd
RSijelmass <R.Sijelmass@gmail.com>
2017-08-02 17:38:42 +0100

    Creates basic interface - console logs which button has been pressed

---
+++ interface.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById('X').addEventListener('click', function() {
+		console.log('You chose X!');
+	});
+
+	document.getElementById('O').addEventListener('click', function() {
+		console.log('You chose O!');
+	});
+
+
+
+});
