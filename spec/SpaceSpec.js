3bb24876bd82f332d322bed8cf0d9e5dddca7eb2
RSijelmass <R.Sijelmass@gmail.com>
2017-08-02 17:18:04 +0100

    Creates #fillin for object Space - user can fill in a space with X or O, with other value an error is   thrown

---
+++ spec/SpaceSpec.js
@@ -0,0 +1,18 @@
+describe('Space', function() {
+	
+	var space;
+	beforeEach(function() {
+		space = new Space();
+	});
+	describe('takes in parameter', function() {
+		it('takes in X', function() {
+			expect(space.fillIn('X')).toEqual('X')
+		});
+	it('takes in O', function() {
+			expect(space.fillIn('O')).toEqual('O')
+		});
+	it('doesn\'t take in other value', function() {
+			expect( function() { space.fillIn('R') } ).toThrow('This move is not allowed!')
+		});
+	});
+});
---
+++ src/Space.js
@@ -0,0 +1,8 @@
+Space = function() {
+	this.value = undefined;
+
+	this.fillIn = function(move) {
+		if(move === 'X' || move === 'O') { return this.value = move };
+		throw 'This move is not allowed!'
+	};
+};
