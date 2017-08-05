36b617ab484ed46f2beace640cec29b5c511df88
RSijelmass <R.Sijelmass@gmail.com>
2017-08-05 12:18:15 +0100

    Checks for diagonal win -tested XXX from top left to lower right

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -63,5 +63,11 @@ describe('Board', function() {
 			var board = new Board(['','','O','','','O','','','O'])
 			expect(board.isWin()).toEqual(true)
 		});
-});
+	});
+	describe('checks for a diagonal win', function() {
+		it('finds XXX win from top left to lower right', function() {
+			var board = new Board(['X','','','','X','','','','X'])
+			expect(board.isWin()).toEqual(true)
+		});
+	});
 });
