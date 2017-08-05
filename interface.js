2fffe394c6b87908ae7a277862f79ad934dc6597
RSijelmass <R.Sijelmass@gmail.com>
2017-08-05 18:45:26 +0100

    Refactors interface.js - for loop to add event listeners to each space - make if/else statement one-line swap method

--- interface.js
+++ interface.js
@@ -1,27 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	var currentMove = 'X'
+	var currentMove = 'X';
+	var nextMove = 'O';
 
 	function chooseSpace(space)  {
 		document.getElementById(space).addEventListener('click', function() {
 			document.getElementById(space).innerHTML = currentMove
-			if(currentMove === 'X') {
-				currentMove = 'O' 
-			} else {
-				currentMove = 'X'
-			};
+			currentMove = [nextMove, nextMove = currentMove][0];
 		});
 	}
 
+	for(var i=1; i<10; i++) {
+		document.getElementById(i.toString()).addEventListener('click', chooseSpace(i.toString()));
+	}
 
-	document.getElementById('1').addEventListener('click', chooseSpace('1'));
-	document.getElementById('2').addEventListener('click', chooseSpace('2'));
-	document.getElementById('3').addEventListener('click', chooseSpace('3'));
-	document.getElementById('4').addEventListener('click', chooseSpace('4'));
-	document.getElementById('5').addEventListener('click', chooseSpace('5'));
-	document.getElementById('6').addEventListener('click', chooseSpace('6'));
-	document.getElementById('7').addEventListener('click', chooseSpace('7'));
-	document.getElementById('8').addEventListener('click', chooseSpace('8'));
-	document.getElementById('9').addEventListener('click', chooseSpace('9'));
-
-});
+	});
