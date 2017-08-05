c3d631334790170ff312e8a120d3fe0adb1f916a
RSijelmass <R.Sijelmass@gmail.com>
2017-08-05 12:41:59 +0100

    Refactors Board function - creates functions #isHorizontalWin, #isVerticalWin and #isDiagonalWin - removes checkHorizontal / checkVertical / checkDiagonal

--- src/Board.js
+++ src/Board.js
@@ -2,18 +2,27 @@ Board = function(spaces) {
 	this.spaces = spaces;
 
 	this.isWin = function() {
-		if(checkHorizontal(0,3) || checkHorizontal(3,6) || checkHorizontal(6,9)) { return true }
-		if(checkVertical(0,3,6) || checkVertical(1,4,7) || checkVertical(2,5,8)) { return true };
-		if(checkVertical(0,4,8) || checkVertical(2,4,6)) { return true };
+		if(isHorizontalWin() || isVerticalWin() || isDiagonalWin()) { return true };
 	};
 
-	checkHorizontal = function(startLine, endLine) {
-		var line = spaces.slice(startLine, endLine).join()
-		if(line === 'X,X,X' || line === 'O,O,O') { return true };
-	};
-	
-	checkVertical = function(spaceOne, spaceTwo, spaceThree) {
+	checkCombination = function(spaceOne, spaceTwo, spaceThree) {
 		var line = `${spaces[spaceOne]},${spaces[spaceTwo]},${spaces[spaceThree]}`
 		if(line === 'X,X,X' || line === 'O,O,O') { return true };
+	};
+
+	isHorizontalWin = function() {
+		if(checkCombination(0,1,2) || checkCombination(3,4,5) || checkCombination(6,7,8)) { 
+			return true 
+		};
+	};
+	
+	isVerticalWin = function() {
+		if(checkCombination(0,3,6) || checkCombination(1,4,7) || checkCombination(2,5,8)) { 
+			return true 
+		};
+	};
+
+	isDiagonalWin = function() {
+		if(checkCombination(0,4,8) || checkCombination(2,4,6)) { return true };
 	};
 };
